aca6a94156b7c4782d3695a08c319adb114d7b38
ReiPano <reipano33@yahoo.com>
2023-08-28 18:29:23 +0200

    Added redirect

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '',   redirectTo: '/auth/signin', pathMatch: 'full' },
   {path: 'signin', loadChildren: () => import('./signin/signin.module').then(m => m.SigninModule)},
   {path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule)},
 ];
