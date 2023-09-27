ffb307ba5796c3caecfff9ad14f3d0aaf8cf1416
ReiPano <reipano33@yahoo.com>
2023-09-27 17:56:03 +0200

    Reverted changes

--- src/app/common/components/side-bar/side-bar.component.ts
+++ src/app/common/components/side-bar/side-bar.component.ts
@@ -1,13 +1,11 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthStateService } from '../../services/auth-state.service';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'asrdb-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SideBarComponent {
   sideBarElements = [
@@ -16,21 +14,18 @@ export class SideBarComponent {
       sectionElements: [
         {
           title: "Dashboard",
-          path: "/dashboard/overview",
+          path: "dashboard/overview",
           icon: "dashboard",
-          isSelected: false,
         },
         {
           title: "Building register",
           path: "/dashboard/buildings-register",
           icon: "domainm",
-          isSelected: false,
         },
         {
           title: "Quality management",
           path: "/dashboard/quality-management",
           icon: "edit_note",
-          isSelected: false,
         },
       ]
     },
@@ -41,13 +36,11 @@ export class SideBarComponent {
           title: "User administration",
           path: "/dashboard/administration/user-management",
           icon: "manage_accounts",
-          isSelected: false,
         },
         {
           title: "Role management",
           path: "/dashboard/administration/role-management",
           icon: "verified_user",
-          isSelected: false,
         },
       ],
       condition: (): boolean => {
@@ -56,32 +49,14 @@ export class SideBarComponent {
     }
   ]
 
-  constructor(private authStateService: AuthStateService, private router: Router, private changeDetection: ChangeDetectorRef) {
-    setTimeout(() => {
-      this.updateIsSelectedValues(this.router.url);
-      changeDetection.markForCheck();
-    }, 100);
-    this.router.events.pipe(
-      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
-      map(e => {
-        this.updateIsSelectedValues(e.url);
-      })
-    );
+  constructor(private authStateService: AuthStateService, private router: Router) {
   }
 
   get isAdmin(): boolean {
     return this.authStateService.isAdmin();
   }
 
-  private updateIsSelectedValues(url: string) {
-    for (const sideBarElement of this.sideBarElements) {
-      for (const sectionElement of sideBarElement.sectionElements) {
-        sectionElement.isSelected = this.isSelected(sectionElement.path, url);
-      }
-    }
-  }
-
-  private isSelected(path: string, url: string): boolean {
-    return url.includes(path);
+  isSelected(path: string): boolean {
+    return this.router.url.includes(path);
   }
 }
