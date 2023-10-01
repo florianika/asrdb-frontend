b02b805d6184f783720aaf08c09bfbca53895f35
ReiPano <reipano33@yahoo.com>
2023-10-01 13:55:21 +0200

    Fixed bug with sidebar

--- src/app/common/components/side-bar/side-bar.component.ts
+++ src/app/common/components/side-bar/side-bar.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { AuthStateService } from '../../services/auth-state.service';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/internal/operators/filter';
 
 @Component({
   selector: 'asrdb-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SideBarComponent {
+export class SideBarComponent implements AfterViewInit {
   sideBarElements = [
     {
       sectionTitle: "",
@@ -16,18 +18,22 @@ export class SideBarComponent {
           title: "Dashboard",
           path: "dashboard/overview",
           icon: "dashboard",
+          selected: false
         },
         {
           title: "Building register",
           path: "/dashboard/buildings-register",
           icon: "domainm",
+          selected: false
         },
         {
           title: "Quality management",
           path: "/dashboard/quality-management",
           icon: "edit_note",
+          selected: false
         },
-      ]
+      ],
+      visible: true
     },
     {
       sectionTitle: "Administration",
@@ -36,27 +42,45 @@ export class SideBarComponent {
           title: "User administration",
           path: "/dashboard/administration/user-management",
           icon: "manage_accounts",
+          selected: false
         },
         {
           title: "Role management",
           path: "/dashboard/administration/role-management",
           icon: "verified_user",
+          selected: false
         },
       ],
-      condition: (): boolean => { //TODO: This is triggered on hover. Find a better solution
-        return this.isAdmin;
-      }
+      visible: this.isAdmin
     }
   ]
 
-  constructor(private authStateService: AuthStateService, private router: Router) {
+  constructor(private authStateService: AuthStateService, private router: Router, private changeDetection: ChangeDetectorRef) {
+  }
+
+  ngAfterViewInit(): void {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.setSelected();
+        this.changeDetection.detectChanges();
+      });
   }
 
   get isAdmin(): boolean {
     return this.authStateService.isAdmin();
   }
 
-  isSelected(path: string): boolean {
+  private setSelected() {
+    for (const sideBarElement of this.sideBarElements) {
+      for (const sectionElement of sideBarElement.sectionElements) {
+        sectionElement.selected = this.isSelected(sectionElement.path);
+      }
+    }
+    console.log(this.sideBarElements)
+  }
+
+  private isSelected(path: string): boolean {
     return this.router.url.includes(path);
   }
 }
