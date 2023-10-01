1e9356c6bc7cbf368dcd548aa8d96f61f76eb981
ReiPano <reipano33@yahoo.com>
2023-10-01 15:05:22 +0200

    Added graphs placeholders

---
+++ src/app/dashboard/overview/components/maps/maps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'asrdb-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+export class MapsComponent {
+
+}
--- src/app/dashboard/overview/overview.module.ts
+++ src/app/dashboard/overview/overview.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { OverviewRoutingModule } from './overview-routing.module';
 import { OverviewComponent } from './overview.component';
+import { PlotlyModule } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { GraphsComponent } from './components/graphs/graphs.component';
+import { MapsComponent } from './components/maps/maps.component';
+import { TablesComponent } from './components/tables/tables.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
-    OverviewComponent
+    OverviewComponent,
+    GraphsComponent,
+    MapsComponent,
+    TablesComponent
   ],
   imports: [
     CommonModule,
-    OverviewRoutingModule
+    OverviewRoutingModule,
+    PlotlyModule,
+    MatCardModule,
+    MatGridListModule
   ]
 })
 export class OverviewModule { }
