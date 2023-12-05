36544ec243df99bb9760aee07f0b632e83676578
ReiPano <reipano33@yahoo.com>
2023-12-05 18:18:18 +0100

    Added building view

--- src/app/dashboard/buildings-register/building/building-details/building-details.component.ts
+++ src/app/dashboard/buildings-register/building/building-details/building-details.component.ts
@@ -14,6 +14,170 @@ import { CommonBuldingRegisterHelper } from 'src/app/dashboard/common/service/co
 export class BuildingDetailsComponent implements OnInit {
   isLoadingResults = true;
   building: any;
+
+  sections = [
+    {
+      title: 'General details',
+      entries: [
+        {
+          title: '',
+          propName: 'BldMunicipality',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldCadastralZone',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldAddressID',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldCensus2023',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldEnumArea',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldType',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldStatus',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldClass',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldProperty',
+          value: ''
+        },
+      ]
+    },
+    {
+      title: 'Building measurements',
+      entries: [
+        {
+          title: '',
+          propName: 'BldArea',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldVolume',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldHeight',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldFloorsAbove',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldFloorsUnder',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldDwellingRecs',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldEntranceRecs',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldLatitude',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldLongitude',
+          value: ''
+        },
+      ]
+    },
+    {
+      title: 'Building utilities',
+      entries: [
+        {
+          title: '',
+          propName: 'BldElectricity',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldElevator',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldPipedGas',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldPipedWater',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldRainWater',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldWasteWater',
+          value: ''
+        },
+      ]
+    },
+    {
+      title: 'Other details',
+      entries: [
+        {
+          title: '',
+          propName: 'BldYearConstruction',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldYearDemolition',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldPermitDate',
+          value: ''
+        },
+        {
+          title: '',
+          propName: 'BldPermitNumber',
+          value: ''
+        },
+      ]
+    }
+  ]
+
   private subscriber = new Subject();
   private fields: any[] = [];
   private id: string | null = '';
@@ -34,7 +198,7 @@ export class BuildingDetailsComponent implements OnInit {
   }
 
   getValueFromStatus(column: string) {
-    return this.commonBuildingRegisterHelper.getValueFromStatus(this.fields, column, this.building[column]) ?? "-";
+    return this.commonBuildingRegisterHelper.getValueFromStatus(this.fields, column, this.building[column]) ?? "Unknown";
   }
 
   private prepareWhereCase() {
@@ -52,7 +216,17 @@ export class BuildingDetailsComponent implements OnInit {
       this.fields = res.data.fields;
     }
     this.building = res.data.features.map((feature: any) => feature.attributes)[0];
+    this.fillSections();
     this.isLoadingResults = false;
+  }
+
+  private fillSections() {
+    this.sections.forEach(section => {
+      section.entries.forEach(entry => {
+        entry.title = this.getTitle(entry.propName);
+        entry.value = entry.propName === 'BldMunicipality' ? this.getMunicipality() : this.getValueFromStatus(entry.propName);
+      });
+    });
   }
 
   private loadBuildings() {
