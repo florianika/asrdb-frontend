a6a21518501385e7be52540cf1ee42af9ae3c249
reipano <rei.pano@gsgcontractor.com>
2024-03-25 22:56:39 +0100

    Updated endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   base_url: 'http://51.107.11.117:9090',
-  building_url: 'https://gislab.teamdev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/1',
-  entrance_url: 'https://gislab.teamdev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/0',
-  dwelling_url: 'https://gislab.teamdev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/2'
+  building_url: 'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/1',
+  entrance_url: 'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/0',
+  dwelling_url: 'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/2'
 };
