e3e2106064d2a1629b52ce69fc1e2180b81efa64
reipano <rei.pano@gsgcontractor.com>
2025-06-01 16:02:11 +0200

    - refactored delete confirmation popup and extracted it to a generic component

---
+++ src/app/dashboard/common/components/entity-delete-confirmation-doalog/entity-delete-confirmation-dialog.component.ts
@@ -0,0 +1,88 @@
+import {Component, Inject} from '@angular/core';
+import {AsyncPipe, NgIf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent, MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {takeUntil} from "rxjs";
+import {RegisterDeleteService} from "../../../register/register-table-view/register-delete.service";
+import {EntityType} from "../../../quality-management/quality-management-config";
+
+export type EntityDeleteDialogData = { type: EntityType, idToDelete: string, reload: () => void };
+
+@Component({
+  selector: 'asrdb-entity-delete-confirmation-doalog',
+  standalone: true,
+  imports: [
+      AsyncPipe,
+      MatButton,
+      MatDialogActions,
+      MatDialogClose,
+      MatDialogContent,
+      MatDialogTitle,
+      MatProgressSpinner,
+      NgIf
+  ],
+  providers: [RegisterDeleteService],
+  templateUrl: './entity-delete-confirmation-dialog.component.html',
+  styleUrl: './entity-delete-confirmation-dialog.component.css'
+})
+export class EntityDeleteConfirmationDialogComponent {
+  private deleteDialog?: MatDialogRef<any>;
+  private readonly idToDelete?: string;
+
+  disableDialogButtons = false;
+  loadingDeleteData;
+  toDeleteState;
+  reload: () => void;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: EntityDeleteDialogData,
+    private registerDeleteService: RegisterDeleteService) {
+    this.idToDelete = data.idToDelete;
+    this.reload = data.reload;
+    this.loadingDeleteData = this.registerDeleteService.deleteDataLoading;
+    this.toDeleteState = this.registerDeleteService.state;
+
+    switch (data.type) {
+      case 'BUILDING':
+        this.registerDeleteService.deleteBuilding(this.idToDelete);
+        break;
+      case 'ENTRANCE':
+        this.registerDeleteService.deleteEntrance(this.idToDelete);
+        break;
+      case 'DWELLING':
+        this.registerDeleteService.deleteDwelling(this.idToDelete);
+        break;
+      default:
+        throw new Error(`Unknown entity type: ${data.type}`);
+    }
+  }
+
+  handleCancelClick() {
+    this.disableDialogButtons = false;
+    this.deleteDialog?.close();
+    this.registerDeleteService.reset();
+  }
+
+  handleDeleteConfirm() {
+    if (!this.idToDelete) {
+      return;
+    }
+    this.disableDialogButtons = true;
+    this.registerDeleteService.confirmDelete();
+    this.registerDeleteService.deleteDone
+      .subscribe(deleted => {
+        if (deleted.buildingDone && deleted.entranceDone && deleted.dwellingDone) {
+          this.reload();
+          this.deleteDialog?.close();
+          this.disableDialogButtons = false;
+        }
+      });
+  }
+}
