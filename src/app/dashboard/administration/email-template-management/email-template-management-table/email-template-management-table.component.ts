717749546f135c20bd991d38d6de493743392520
reipano <rei.pano@gsgcontractor.com>
2025-06-15 19:04:52 +0200

    - Email templates table will show usernames instead of user id

--- src/app/dashboard/administration/email-template-management/email-template-management-table/email-template-management-table.component.ts
+++ src/app/dashboard/administration/email-template-management/email-template-management-table/email-template-management-table.component.ts
@@ -1,10 +1,11 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {map, Observable} from 'rxjs';
+import {combineLatestWith, Observable} from 'rxjs';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {EmailTemplateManagementService} from '../email-template-management.service';
 import {EmailTemplate} from '../../../../model/EmailTemplate.model';
+import {UserManagementService} from "../../user-management/user-management.service";
 
 @Component({
   selector: 'asrdb-email-template-management-table',
@@ -21,14 +22,7 @@ export class EmailTemplateManagementTableComponent
     'createdTimestamp',
     'actions',
   ];
-  dataSourceObservable: Observable<MatTableDataSource<EmailTemplate>> =
-    this.emailTemplateManagementService.emailTemplatesAsObservable.pipe(
-      map(users => {
-        const dataSource = this.dataSource;
-        dataSource.data = users;
-        return dataSource;
-      })
-    );
+  dataSourceObservable: Observable<MatTableDataSource<EmailTemplate>> | null = null;
 
   private dataSource: MatTableDataSource<EmailTemplate> =
     new MatTableDataSource<EmailTemplate>();
@@ -40,10 +34,31 @@ export class EmailTemplateManagementTableComponent
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
-    private emailTemplateManagementService: EmailTemplateManagementService
-  ) {}
+    private emailTemplateManagementService: EmailTemplateManagementService,
+    private userManagementService: UserManagementService
+  ) {
+    this.emailTemplateManagementService.emailTemplatesAsObservable
+      .pipe(combineLatestWith(this.userManagementService.usersAsObservable))
+      .subscribe(([emails, users]) => {
+        this.dataSource.data = emails?.map((email) => {
+          const user = users.find(
+            (user) => user.id === email.createdUser
+          );
+          return {
+            ...email,
+            createdUser: user ? user.name + ' ' + user.lastName : 'Unknown User',
+          };
+        });
+        this.resultsLength = this.dataSource.data.length;
+        this.dataSourceObservable = new Observable((observer) => {
+          observer.next(this.dataSource);
+          observer.complete();
+        });
+      });
+  }
 
   ngOnInit(): void {
+    this.userManagementService.getUsers();
     this.emailTemplateManagementService.getEmailTemplates();
   }
 
--- src/app/dashboard/administration/email-template-management/email-template-management.module.ts
+++ src/app/dashboard/administration/email-template-management/email-template-management.module.ts
@@ -21,6 +21,7 @@ import { EmailTemplateManagementService } from './email-template-management.serv
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { ConcatenateMessagePipe } from '../../register/register-log-view/register-log-table/register-log-message.pipe';
 import { NgxEditorModule } from 'ngx-editor';
+import {UserManagementService} from "../user-management/user-management.service";
 
 @NgModule({
   declarations: [
@@ -48,6 +49,6 @@ import { NgxEditorModule } from 'ngx-editor';
     ConcatenateMessagePipe,
     NgxEditorModule,
   ],
-  providers: [EmailTemplateManagementService],
+  providers: [EmailTemplateManagementService, UserManagementService],
 })
 export class EmailTemplateManagementModule {}
