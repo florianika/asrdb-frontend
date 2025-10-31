a61454b342a1d98554507df1cc97afe6f8fd9c1e
reipano <rei.pano@gsgcontractor.com>
2025-10-31 19:13:25 +0100

    - added validation on street creation - added validation on street edit

--- src/app/dashboard/register/register-form/street-creation.service.ts
+++ src/app/dashboard/register/register-form/street-creation.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, catchError, of} from 'rxjs';
 import { EntityManageResponse } from '../model/entity-req-res';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Dwelling } from '../model/dwelling';
 import { AuthStateService } from '../../../common/services/auth-state.service';
 import { CommonStreetService } from '../../common/service/common-street.service';
 import { Street } from '../model/street';
+import {QueryFilter} from "../model/query-filter";
 
 @Injectable()
 export class StreetManagementService {
@@ -46,10 +47,49 @@ export class StreetManagementService {
 
   public saveStreet(streetDetails: Street) {
     if (streetDetails.GlobalID) {
-      this.updateStreet(streetDetails);
+      this.validateStreetNameUniqueness(
+        streetDetails.StrNameCore,
+        streetDetails.StrMunicipality,
+        () => this.updateStreet(streetDetails),
+        streetDetails.GlobalID
+      )
     } else {
-      this.createStreet(streetDetails);
+      this.validateStreetNameUniqueness(
+        streetDetails.StrNameCore,
+        streetDetails.StrMunicipality,
+        () => this.createStreet(streetDetails)
+      )
     }
+  }
+
+  private validateStreetNameUniqueness(name: string, municipality: number, callback: () => void, globalId?: string) {
+    let whereClause = "StrMunicipality=" + municipality + " AND StrNameCore='" + name + "'";
+    if (globalId) {
+      whereClause += " AND GlobalID<>'" + globalId + "'";
+    }
+    const filter = {
+      where: whereClause,
+      start: 0,
+      num: 1,
+      outFields: ['GlobalID'],
+    } as QueryFilter;
+    this.commonStreetService.getStreets(filter)
+      .pipe(catchError(() => of(null)))
+      .subscribe((data) => {
+        if (!data) {
+          this.snackBar.open('Could not validate street name uniqueness. Please try again.', 'Ok', {
+            duration: 5000,
+          });
+          return;
+        }
+        if (data && data.count > 0) {
+          this.snackBar.open('A street with this name already exists in the selected municipality.', 'Ok', {
+            duration: 5000,
+          });
+          return;
+        }
+        callback();
+      })
   }
 
   private createStreet(street: Street) {
