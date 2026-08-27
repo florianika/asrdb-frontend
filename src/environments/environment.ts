6659d4fa9930caef6ece618baee0a6c523a14866
reipano <rei.pano@gsgcontractor.com>
2026-08-27 13:33:53 +0200

    fix(environment): update URLs to production endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
-  base_url: 'http://51.107.11.117:9090',
+  base_url: 'https://api-asrbd.instat.gov.al:9443',
   portal_url: 'https://salstatstaging.tddev.it/portal',
   building_url:
-    'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/1',
+    'https://asrbd.instat.gov.al/server/rest/services/SALSTAT/asrbd/FeatureServer/1',
   entrance_url:
-    'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/0',
+    'https://asrbd.instat.gov.al/server/rest/services/SALSTAT/asrbd/FeatureServer/0',
   dwelling_url:
-    'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/2',
+    'https://asrbd.instat.gov.al/server/rest/services/SALSTAT/asrbd/FeatureServer/2',
   street_url:
-    'https://salstatstaging.tddev.it/arcgis/rest/services/SALSTAT/asrbd/FeatureServer/3',
+    'https://asrbd.instat.gov.al/server/rest/services/SALSTAT/asrbd/FeatureServer/3',
   municipality_url:
     'https://services7.arcgis.com/E9FE1JuiACmTPbPv/ArcGIS/rest/services/Municipality/FeatureServer/7',
   token_url:
     'https://salstatstaging.tddev.it/portal/sharing/rest/generateToken',
-  hangfire_url: 'http://51.107.11.117:9090/hangfire/jobs/'
+  hangfire_url: 'https://api-asrbd.instat.gov.al:9443/hangfire/jobs/'
 };
